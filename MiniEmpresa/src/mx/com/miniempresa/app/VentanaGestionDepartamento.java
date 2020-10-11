4361aebbf4cff3405807f9b7d4803f385a5036be
Jaime Cano Martinez <jckano@MacBook-Pro-de-Jaime.local>
2020-10-11 10:07:25 -0500

    TallerJava.- Se corrige clase VentanaGestionDepartamental

--- MiniEmpresa/src/mx/com/miniempresa/app/VentanaGestionDepartamento.java
+++ MiniEmpresa/src/mx/com/miniempresa/app/VentanaGestionDepartamento.java
@@ -1,17 +1,10 @@
 package mx.com.miniempresa.app;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,35 +15,11 @@ import mx.com.miniempresa.model.Empleado;
 import mx.com.miniempresa.service.GestionDepartamentalBO;
 
 public class VentanaGestionDepartamento extends JFrame{
-	
-
-	public VentanaGestionDepartamento() {//se genera el constructor
-		super("Ventana Gestion Departamento");//se manda a llamar el constructor de su papá(o sea el JFrame)
-		getContentPane().setLayout(new FlowLayout());
-		inicializarComponentes();
-		setSize(700,500);
-	}
-	
-	private void inicializarComponentes() {
-		JLabel etiqueta = new JLabel("Nombre");
 		
-		JTextField campoTexto = new JTextField("Ingresa Nombre",20);
-		
-		JButton boton = new JButton("Ejecutar");
-		
-		//listeners
-		boton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Empleado nuevoEmpleado = new Empleado();
-				nuevoEmpleado.setNombre(campoTexto.getText());
-				
-			}
-		});
-		
-
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private GestionDepartamentalBO gestionDepartamentalBO = GestionDepartamentalBO.generarInstancia();
 	
 	public VentanaGestionDepartamento() {
