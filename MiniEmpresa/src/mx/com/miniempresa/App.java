809ce328502212ae238f684c0886863cdf170e40
Jaime Cano Martinez <jckano@MacBook-Pro-de-Jaime.local>
2020-10-11 10:30:32 -0500

    Clase principal para llamado de ventanas

--- MiniEmpresa/src/mx/com/miniempresa/App.java
+++ MiniEmpresa/src/mx/com/miniempresa/App.java
@@ -1,15 +1,21 @@
 package mx.com.miniempresa;
 
 import mx.com.miniempresa.app.VentanaGestionClientes;
+import mx.com.miniempresa.app.VentanaGestionVentas;
 
 public class App {
 
 	public static void main(String[] args) {
+		//Adrian
+		//VentanaGestionClientes ventanaClientes = new VentanaGestionClientes();
+		//ventanaClientes.show();
 		
-		VentanaGestionClientes ventanaClientes = new VentanaGestionClientes();
-		ventanaClientes.show();
+		//Itzel
+		//VentanaGestionVentas ventanaVentas = new VentanaGestionVentas();
+		//ventanaVentas.show();
 		
-		
+		//Ivonne, Rodrigo, Karen, Monse, Carlos
+		//=(
 	}
 
 }
